f11d7fc56f9f1c8fbce488a3400b1896b96fe06f
yashvimehta <yashvimehta45@gmail.com>
2021-11-22 20:59:37 +0530

    login signup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+//var popup = require('popups');
+//import swal from 'sweetalert';
+
 
 class AuthController{
 	constructor(){}
@@ -38,7 +41,7 @@ class AuthController{
 									console.log('Success');
 							});
 						}).sort({_id: -1}).limit(1);
-						res.send({"Success":"You are regestered,You can login now."});
+						res.render("/profile");
 					}else{
 						res.send({"Success":"Email is already used."});
 					}
@@ -53,9 +56,11 @@ class AuthController{
 			if(data){
 				if(data.password==req.body.password){
 					req.session.userId = data.unique_id;
-					res.send({"Success":"Success!"});
+					//console.log(req.session.userId);
+					res.redirect('/profile');
 				}else{
-					res.send({"Success":"Wrong password!"});
+					res.send({"Success":"Incorrect password"});
+					
 				}
 			}else{
 				res.send({"Success":"This Email Is not regestered!"});
@@ -66,6 +71,7 @@ class AuthController{
 		return res.render('login.ejs');
 	}
 	profile (req, res, next) {
+		console.log("data");
 		User.findOne({unique_id:req.session.userId},function(err,data){
 			console.log("data");
 			console.log(data);
@@ -120,7 +126,53 @@ class AuthController{
 
 const AuthCtrl = new AuthController();
 
-router.get('/', AuthCtrl.indexGet);
+router.get('/', function (req, res, next) {
+	return res.render('index.ejs');
+});
+
+router.post('/', function(req, res, next) {
+	console.log(req.body);
+	var personInfo = req.body;
+	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+			User.findOne({email:personInfo.email},function(err,data){
+				if(!data){
+					var c;
+					User.findOne({},function(err,data){
+						if (data) {
+							console.log("if");
+							c = data.unique_id + 1;
+						}else{
+							c=1;
+						}
+						var newPerson = new User({
+							unique_id:c,
+							email:personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
+						newPerson.save(function(err, Person){
+							if(err)
+								console.log(err);
+							else
+								console.log('Success');
+								//res.redirect("/profile");
+						});
+					}).sort({_id: -1}).limit(1);
+					//res.send({"Success":"You are regestered,You can login now."});
+					res.redirect("/profile");
+				}else{
+					res.send({"Success":"Email is already used."});
+				}
+			});
+		} else {
+			res.send({"Success":"password is not matched"});
+		}
+	}
+});
 
 router.post('/', AuthCtrl.indexPost);
 
