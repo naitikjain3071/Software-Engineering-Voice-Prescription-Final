9074590f96e89b859c7348e53869b90e35976ea6
naitikjain3071 <70900997+naitikjain3071@users.noreply.github.com>
2021-11-22 14:36:09 +0530

    Commit

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,50 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+class AuthController{
+	constructor(){}
+	login(req, res, next) {
+	//console.log(req.body);
+		User.findOne({email:req.body.email},function(err,data){
+			if(data){
+				if(data.password==req.body.password){
+					//console.log("Done Login");
+					req.session.userId = data.unique_id;
+					//console.log(req.session.userId);
+					res.send({"Success":"Success!"});
+				}else{
+					res.send({"Success":"Wrong password!"});
+				}
+			}else{
+				res.send({"Success":"This Email Is not regestered!"});
+			}
+		});
+	}
+}
+
+const AuthCtrl = new AuthController();
+
 router.get('/', function (req, res, next) {
 	return res.render('index.ejs');
 });
 
-
 router.post('/', function(req, res, next) {
 	console.log(req.body);
 	var personInfo = req.body;
-
-
 	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
 		res.send();
 	} else {
 		if (personInfo.password == personInfo.passwordConf) {
-
 			User.findOne({email:personInfo.email},function(err,data){
 				if(!data){
 					var c;
 					User.findOne({},function(err,data){
-
 						if (data) {
 							console.log("if");
 							c = data.unique_id + 1;
 						}else{
 							c=1;
 						}
-
 						var newPerson = new User({
 							unique_id:c,
 							email:personInfo.email,
@@ -36,22 +53,19 @@ router.post('/', function(req, res, next) {
 							password: personInfo.password,
 							passwordConf: personInfo.passwordConf
 						});
-
 						newPerson.save(function(err, Person){
 							if(err)
 								console.log(err);
 							else
 								console.log('Success');
 						});
-
 					}).sort({_id: -1}).limit(1);
 					res.send({"Success":"You are regestered,You can login now."});
 				}else{
 					res.send({"Success":"Email is already used."});
 				}
-
 			});
-		}else{
+		} else {
 			res.send({"Success":"password is not matched"});
 		}
 	}
@@ -61,25 +75,7 @@ router.get('/login', function (req, res, next) {
 	return res.render('login.ejs');
 });
 
-router.post('/login', function (req, res, next) {
-	//console.log(req.body);
-	User.findOne({email:req.body.email},function(err,data){
-		if(data){
-			
-			if(data.password==req.body.password){
-				//console.log("Done Login");
-				req.session.userId = data.unique_id;
-				//console.log(req.session.userId);
-				res.send({"Success":"Success!"});
-				
-			}else{
-				res.send({"Success":"Wrong password!"});
-			}
-		}else{
-			res.send({"Success":"This Email Is not regestered!"});
-		}
-	});
-});
+router.post('/login', AuthCtrl.login);
 
 router.get('/profile', function (req, res, next) {
 	console.log("profile");
