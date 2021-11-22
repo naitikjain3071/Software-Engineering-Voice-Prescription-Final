7ce98f6f778ec4331d8f9b49a0349fd21661972b
naitikjain3071 <70900997+naitikjain3071@users.noreply.github.com>
2021-11-22 21:40:53 +0530

    Changes made

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ class AuthController{
 									console.log('Success');
 							});
 						}).sort({_id: -1}).limit(1);
-						res.render("/profile");
+						res.render("/prescription");
 					}else{
 						res.send({"Success":"Email is already used."});
 					}
@@ -57,7 +57,7 @@ class AuthController{
 				if(data.password==req.body.password){
 					req.session.userId = data.unique_id;
 					//console.log(req.session.userId);
-					res.redirect('/profile');
+					res.redirect('/prescription');
 				}else{
 					res.send({"Success":"Incorrect password"});
 					
@@ -70,7 +70,7 @@ class AuthController{
 	loginGet (req, res, next) {
 		return res.render('login.ejs');
 	}
-	profile (req, res, next) {
+	prescription (req, res, next) {
 		console.log("data");
 		User.findOne({unique_id:req.session.userId},function(err,data){
 			console.log("data");
@@ -119,60 +119,14 @@ class AuthController{
 			}
 			}
 		});
-		
 	}
-
 }
 
 const AuthCtrl = new AuthController();
 
-router.get('/', function (req, res, next) {
-	return res.render('index.ejs');
-});
+router.get('/', AuthCtrl.indexGet);
 
-router.post('/', function(req, res, next) {
-	console.log(req.body);
-	var personInfo = req.body;
-	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
-		res.send();
-	} else {
-		if (personInfo.password == personInfo.passwordConf) {
-			User.findOne({email:personInfo.email},function(err,data){
-				if(!data){
-					var c;
-					User.findOne({},function(err,data){
-						if (data) {
-							console.log("if");
-							c = data.unique_id + 1;
-						}else{
-							c=1;
-						}
-						var newPerson = new User({
-							unique_id:c,
-							email:personInfo.email,
-							username: personInfo.username,
-							password: personInfo.password,
-							passwordConf: personInfo.passwordConf
-						});
-						newPerson.save(function(err, Person){
-							if(err)
-								console.log(err);
-							else
-								console.log('Success');
-								//res.redirect("/profile");
-						});
-					}).sort({_id: -1}).limit(1);
-					//res.send({"Success":"You are regestered,You can login now."});
-					res.redirect("/profile");
-				}else{
-					res.send({"Success":"Email is already used."});
-				}
-			});
-		} else {
-			res.send({"Success":"password is not matched"});
-		}
-	}
-});
+router.post('/', AuthCtrl.indexPost);
 
 router.post('/', AuthCtrl.indexPost);
 
@@ -180,7 +134,7 @@ router.get('/login', AuthCtrl.loginGet);
 
 router.post('/login', AuthCtrl.loginPost);
 
-router.get('/profile', AuthCtrl.profile);
+router.get('/prescription', AuthCtrl.prescription);
 
 router.get('/logout', AuthCtrl.logout);
 
